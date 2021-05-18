66f7c2e57b4f5c9b92a9f0eff20cbc353e5b96a8
fabioatsea <fabio.mercante@gmail.com>
2021-05-18 03:00:39 -0300

    first commmit

---
+++ R/somar.R
@@ -0,0 +1,14 @@
+#' Funcao SOMAR
+#'
+#' Esta funcao sereve para somar dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' somar(3,4)
+#'
+#' @export
+somar <- function(a,b){
+  a+b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' Funcao SOMAR
+#'
+#' Esta funcao sereve para subtrair dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' subtrair(3,4)
+#'
+#' @export
+subtrair <- function(a,b){
+  a-b
+}
